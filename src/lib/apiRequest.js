8e03f0be3d545cedd33eaab490a04255f6756054
Alantitus <alantitus89@gmail.com>
2024-07-28 15:45:23 +0530

    updated final commit

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://primespace-server.onrender.com/api",
   withCredentials: true,
 });
 
